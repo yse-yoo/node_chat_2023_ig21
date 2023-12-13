8537637eb2d81a66a2dcf46359d8f2df0ff08d5d
yse-yoo <youhei_yoshikawa@yse-c.net>
2023-12-13 11:39:51 +0900

    update

--- public/js/chat.js
+++ public/js/chat.js
@@ -6,4 +6,11 @@ function sendMessage() {
     // console.log('send message!!')
     var message = document.getElementById('message').value;
     console.log(message);
+    if (message) {
+        //データを作成
+        var data = { 
+            message: message
+        }
+        socket.emit('chat_message', data);
+    }
 }
