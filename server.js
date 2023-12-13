3d7e759dd803c199721e8ede58232e41085e31ea
yse-yoo <youhei_yoshikawa@yse-c.net>
2023-12-13 10:33:07 +0900

    update

--- server.js
+++ server.js
@@ -5,3 +5,18 @@ const app = express();
 app.use(express.urlencoded({ extended: true }));
 // publicフォルダを、Web公開
 app.use(express.static(__dirname + '/public'));
+
+// HTTPサーバの作成
+const { createServer } = require('node:http');
+const server = createServer(app);
+
+//環境変数読み込み
+const dotenv = require('dotenv');
+dotenv.config();
+const host = process.env.HOST;
+const port = process.env.PORT;
+
+// HTTPサーバー待機
+server.listen(port, host, ()=> {
+    console.log(`listening on http://${host}:${port}`);
+})
